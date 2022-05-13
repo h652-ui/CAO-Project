8393f8975b1875c889da2c0bea3753522c832701
Hamid Muzaffar <hamidmuzaffar49@gmail.com>
2022-05-13 18:06:36 +0500

    SPIFF & attachment

--- Final_Project/Final_Project.ino
+++ Final_Project/Final_Project.ino
@@ -1,6 +1,37 @@
+#include "esp_camera.h"
 #include <WiFi.h>
 #include "time.h"
+#include <FS.h>
+#include <SPIFFS.h>
 #include <ESP_Mail_Client.h>
+
+/*Define Camera Pins*/
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+#else
+#error "Camera model not selected"
+#endif
+
+/*Path and name of file to write*/
+#define FILE_PHOTO "/image.jpg"
 
 /* The WIFI in credentials */
 #define WIFI_SSID "Dell5410"
@@ -32,8 +63,22 @@ void setup() {
   Serial.begin(115200);
   /* Connection with WIFI */
   wifiConnection();
-  /* Setting Time Credentials */
+  
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    ESP.restart();
+  }
+  else {
+    delay(500);
+    Serial.println("SPIFFS mounted successfully");
+  }
+  
+  /* Setting Camera Configuration */
+  cameraConfiguration();
+  /* Setting up Time Credentials */
   getTime();
+  /* Capture Photo and saved to SPIFF */
+  capturePhotoSaveSpiffs();
   /* Sending Mail */
   sendEmail();
 }
@@ -57,15 +102,104 @@ void wifiConnection() {
   Serial.println();
 }
 
+void cameraConfiguration() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound()) {
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+// Check if photo capture was successful
+bool checkPhoto( fs::FS &fs ) {
+  File f_pic = fs.open( FILE_PHOTO );
+  unsigned int pic_sz = f_pic.size();
+  return ( pic_sz > 100 );
+}
+
+// Capture Photo and Save it to SPIFFS
+void capturePhotoSaveSpiffs( void ) {
+  camera_fb_t * fb = NULL; // pointer
+  bool ok = 0; // Boolean indicating if the picture has been taken correctly
+
+  do {
+    // Take a photo with the camera
+    Serial.println("Taking a photo...");
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+
+    // Photo file name
+    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+
+    // Insert the data in the photo file
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.print("The picture has been saved in ");
+      Serial.print(FILE_PHOTO);
+      Serial.print(" - Size: ");
+      Serial.print(file.size());
+      Serial.println(" bytes");
+    }
+    // Close the file
+    file.close();
+    esp_camera_fb_return(fb);
+
+    // check if file has been correctly saved in SPIFFS
+    ok = checkPhoto(SPIFFS);
+  } while ( !ok );
+}
+
 void getTime() {
   struct tm timeinfo;
   bool time_status = true;
   do {
     if (!time_status)
       Serial.println("Failed to obtain time");
+    delay(3000);
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
     time_status = getLocalTime(&timeinfo);
-    delay(2000);
+    delay(3000);
   } while (!time_status);
   Serial.println("Succeed to obtain time");
   Serial.println("Time variables");
@@ -98,7 +232,7 @@ void sendEmail() {
   message.sender.email = SENDER_EMAIL;
   message.subject = "ESP Test Email";
   message.addRecipient("Hamid", RECEIVER_EMAIL);
-  
+
   /*Send HTML message*/
   String htmlMsg = "Hello World - Sent from ESP board<br><br>Time : ";
   htmlMsg.concat(pk_time);
@@ -108,9 +242,9 @@ void sendEmail() {
 
   SMTP_Attachment att;
 
-  /** Set the attachment info e.g. 
-   * file name, MIME type, file path, file storage type,
-   * transfer encoding and content encoding
+  /** Set the attachment info e.g.
+     file name, MIME type, file path, file storage type,
+     transfer encoding and content encoding
   */
   att.descr.filename = "image.jpg";
   att.descr.mime = "image/jpg"; //binary data
